d9786b3e9c460d852db76778cefe53a67c849c32
Mitko Stoyanov <mitakatakata1@gmail.com>
2026-02-07 15:55:20 +0200

    Logo position adjusted

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import CreateBuilding from './pages/CreateBuilding';
 import BuildingView from './pages/BuildingView';
 import FloorPlan from './pages/FloorPlan';
+import logo from './assets/logo.png';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<CreateBuilding />} />
-        <Route path="/building/:buildingId/edit" element={<CreateBuilding />} />
-        <Route path="/building/:buildingId" element={<BuildingView />} />
-        <Route path="/building/:buildingId/floor/:floorNumber" element={<FloorPlan />} />
-      </Routes>
+      <header className="app-header">
+        <Link to="/" className="app-logo-link" aria-label="Go to home">
+          <img src={logo} alt="" className="app-logo" />
+        </Link>
+      </header>
+      <main className="app-main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreateBuilding />} />
+          <Route path="/building/:buildingId/edit" element={<CreateBuilding />} />
+          <Route path="/building/:buildingId" element={<BuildingView />} />
+          <Route path="/building/:buildingId/floor/:floorNumber" element={<FloorPlan />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
